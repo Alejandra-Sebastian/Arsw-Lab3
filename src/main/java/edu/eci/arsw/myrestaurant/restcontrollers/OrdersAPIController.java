87b6f74076f3c8fd33e4075b4f3f983bfdd981d6
Sebastián Reyes <sebastianreyes01@hotmail.com>
2018-02-14 23:42:48 -0500

    Hecha Parte I

--- src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
+++ src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
@@ -19,12 +19,20 @@ package edu.eci.arsw.myrestaurant.restcontrollers;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.model.ProductType;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
+import edu.eci.arsw.myrestaurant.services.RestaurantOrderServices;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,7 +42,35 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
+
+@RestController
+@RequestMapping(value = "/orders")
 public class OrdersAPIController {
+	
+	@Autowired
+	private RestaurantOrderServices ros;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> manejadorGetRecursoOrdersAPI() {
+		try {
+ 			//obtener datos que se enviarán a través del API
+ 			return new ResponseEntity<>(ros.getTablesWithOrders(),HttpStatus.ACCEPTED);
+ 		} catch (Exception ex) {
+ 			Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+ 			return new ResponseEntity<>("Error, no se encontraron mesas", HttpStatus.NOT_FOUND);
+ 		}
+	}
+	
+	@RequestMapping(path = "/{idmesa}", method = RequestMethod.GET)
+	public ResponseEntity<?> manejadorGetIdMesaOrdersAPI(@PathVariable int idmesa) {
+		try {
+ 			//obtener datos que se enviarán a través del API
+ 			return new ResponseEntity<>(ros.getTableOrder(idmesa),HttpStatus.ACCEPTED);
+ 		} catch (Exception ex) {
+ 			Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+ 			return new ResponseEntity<>("Error, no se encontro la mesa",HttpStatus.NOT_FOUND);
+ 		}
+	}
 
     
 }
