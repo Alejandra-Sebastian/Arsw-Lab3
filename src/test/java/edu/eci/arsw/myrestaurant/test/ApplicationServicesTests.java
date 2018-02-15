a766147ecadbbdefafc5ccb6e23e44923305ae87
Sebastián Reyes <sebastianreyes01@hotmail.com>
2018-02-15 16:25:28 -0500

    Añadido test

--- src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
@@ -6,6 +6,8 @@ import edu.eci.arsw.myrestaurant.services.OrderServicesException;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +19,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest()
 public class ApplicationServicesTests {
 
-    
+    @Autowired
     RestaurantOrderServicesStub ros;
 
     
     @Test
     public void contextLoads() throws OrderServicesException{
-        
-        
-        
-        
+    
     }
+    
+    
+    @Test
+    public void debeCalcularCorrectamente() throws OrderServicesException{
+        int precio = ros.calculateTableBill(1);
+        System.out.println(precio);
+        int debeDar = (int) ((3*10000+3*10000*0.19)+(3000+3000*0.19)+(4*1300+4*1300*0.16));
+        int precioEsp = debeDar;
+        assertEquals(precioEsp, precio);
+    }
+    
 
 }
